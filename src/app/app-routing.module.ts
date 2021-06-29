4859a4dc6f453d83255c3f702e840baa86b97228
Alpaslan Dulkadir <alpaslandulkadir@gmail.com>
2021-06-29 23:46:27 +0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoryProductsComponent } from './_components/category-products/category-products.component';
 import { HomeComponent } from './_components/home/home.component';
 import { ProductComponent } from './_components/product/product.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,20 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './_components/nav/nav.component';
 import { CategoryComponent } from './_components/category/category.component';
 import { ProductComponent } from './_components/product/product.component';
-import { CategoryProductsComponent } from './_components/category-products/category-products.component';
 import { HomeComponent } from './_components/home/home.component';
 import { UrlformatPipe } from './_pipes/urlformat.pipe';
 import { ProductDetailComponent } from './_components/product-detail/product-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, CategoryComponent, ProductComponent, CategoryProductsComponent, HomeComponent, UrlformatPipe, ProductDetailComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    CategoryComponent,
+    ProductComponent,
+    HomeComponent,
+    UrlformatPipe,
+    ProductDetailComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
